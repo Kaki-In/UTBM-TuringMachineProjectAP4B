363ccbea24cdb9f21624febfc04c3f46a4f2d63e
Kaki In <kaki@mifamofi.net>
2025-01-03 15:22:05 +0100

    Added the GameWinnersPanel

--- com/turing_machine/views/GameWinnersPanel.java
+++ com/turing_machine/views/GameWinnersPanel.java
@@ -3,24 +3,35 @@ package com.turing_machine.views;
 import com.turing_machine.started_game.StartedGamePlayer;
 import java.awt.Component;
 import java.util.ArrayList;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class GameWinnersPanel implements Displayable {
 
-	private ArrayList<StartedGamePlayer> winners;
+	private final ArrayList<StartedGamePlayer> winners;
+
+    private final JPanel panel;
 
 	public GameWinnersPanel(ArrayList<StartedGamePlayer> winners) {
-        StartedGamePlayer player = winners.get(0);
-        player.getNotes();
+        this.winners = winners;
+
+        this.panel = new JPanel();
+        this.panel.setLayout(new BoxLayout(this.panel, BoxLayout.Y_AXIS));
+
+        for (StartedGamePlayer winner: this.winners)
+        {
+            this.panel.add(new JLabel(winner.getName()));
+        }
 	}
 
     @Override
     public void refresh() {
-        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public Component getWidget() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return this.panel;
     }
 
 }

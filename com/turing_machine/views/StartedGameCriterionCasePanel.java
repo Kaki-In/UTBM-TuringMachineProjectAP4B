22e0d47bf6a006ab420ff7a0a9189e8d89b93be6
Kaki In <kaki@mifamofi.net>
2025-01-03 18:03:34 +0100

    Added the StartedGameCriterionCasePanel

--- com/turing_machine/views/StartedGameCriterionCasePanel.java
+++ com/turing_machine/views/StartedGameCriterionCasePanel.java
@@ -1,7 +1,12 @@
 package com.turing_machine.views;
 
 import com.turing_machine.database.CriterionCase;
+import com.turing_machine.database.CriterionCaseThumbnail;
+import com.turing_machine.database.Database;
+import com.turing_machine.exceptions.NoSuchCaseException;
 import java.awt.Component;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class StartedGameCriterionCasePanel implements Displayable {
@@ -14,6 +19,19 @@ public class StartedGameCriterionCasePanel implements Displayable {
 		this.criterion_case = criterion_case;
 
 		this.panel = new JPanel();
+		this.panel.setLayout(new BoxLayout(this.panel, BoxLayout.Y_AXIS));
+
+		CriterionCaseThumbnail thumbnail;
+		try{
+			thumbnail = Database.getThumbnails().getCasesThumbnails().getCase(criterion_case.getId());
+		} catch (NoSuchCaseException e)
+		{
+			System.err.println("Warning : couldn't find case thumbnail " + criterion_case.getId());
+			return;
+		}
+
+		this.panel.add(new JLabel(new DescriptableIcon(300, 125, thumbnail.getImage())));
+		this.panel.add(new JLabel(thumbnail.getDescription()));
 	}
 
 	@Override

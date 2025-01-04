71464577cf51c34e33977c8384f22c88d39e37a6
Kaki In <kaki@mifamofi.net>
2025-01-04 09:45:08 +0100

    Added missing files

---
+++ com/turing_machine/started_game/StartedGameState.java
@@ -0,0 +1,44 @@
+package com.turing_machine.started_game;
+
+import com.turing_machine.listeners.StartedGameStateListener;
+import java.util.ArrayList;
+
+public class StartedGameState
+{
+    
+	private int round_id;
+
+	private final StartedGameMachine machine;
+	
+	private final ArrayList<StartedGameStateListener> listeners;
+
+    public StartedGameState(StartedGameMachine machine) {
+        this.round_id = 0;
+        this.machine = machine;
+        this.listeners = new ArrayList<>(); 
+    }
+
+    public int getRoundId()
+    {
+        return this.round_id;
+    }
+
+    public StartedGameMachine getMachine()
+    {
+        return this.machine;
+    }
+
+	public void endRound() {
+        ++ this.round_id;
+        for (StartedGameStateListener listener: listeners)
+        {
+            listener.onNewRound(round_id);
+        }
+	}
+
+    public void whenRoundChanged(StartedGameStateListener listener)
+    {
+        this.listeners.add(listener);
+    }
+
+}
